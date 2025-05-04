ebe08afde0aed85e9ae8e06b524bfc90f1354766
corybiscuit <corybiscuit@users.noreply.github.com>
2025-05-03 21:50:27 -0400

    Added starship.toml to script

--- dotfm.sh
+++ dotfm.sh
@@ -34,9 +34,11 @@ a) cd ~/dotfiles
    exec sh "$0" "$@";;
 
 b) rm ~/dotfiles/.zshrc
-   rm -r ~/dotfiles/.config/
+   rm ~/dotfiles/.config/git/config
+   rm ~/dotfiles/.config/starship.toml
    cp -r ~/.zshrc ~/dotfiles/
    cp -r ~/.config/git/config ~/dotfiles/.config/git/
+   cp -r ~/.config/starship.toml ~/dotfiles/.config/
    clear
    exec sh "$0" "$@";;
 
@@ -44,11 +46,13 @@ c) clear
    echo "What config would you like to edit?"
    echo "a) ~/.zshrc"
    echo "b) ~/.config/git/config"
+   echo "c) ~/.config/starship.toml"
    echo "q) Exit"
    read config
         case $config in
         a) nano ~/.zshrc && clear && exec sh "$0" "$@";;
         b) nano ~/.config/git/config && clear && exec sh "$0" "$@";;
+        c) nano ~/.config/starship.toml && clear && exec sh "$0" "$@";;
         q) clear && exec "$0" "$@";;
         *) echo Not one of the choices && sleep 2 && clear && exec sh "$0" "$@";;
         esac
